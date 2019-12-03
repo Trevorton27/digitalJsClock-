71c7aabea5ce655492df168678e94860f44a7294
Trevor Mearns <spiral272@gmail.com>
2019-12-03 15:28:15 +0900

    for safets sake will commit now :-)

--- timeworks.js
+++ timeworks.js
@@ -23,6 +23,40 @@ const theDay = [
     'Sunday'
 ];
 
+const todaysDate = [
+    '1st',
+    '2nd',
+    '3rd',
+    '4th',
+    '5th',
+    '6th',
+    '7th',
+    '8th',
+    '9th',
+    '10th',
+    '11th',
+    '12th',
+    '13th',
+    '14th',
+    '15th',
+    '16th',
+    '17th',
+    '18th',
+    '19th',
+    '20th',
+    '21st',
+    '22nd',
+    '23rd',
+    '24th',
+    '25th',
+    '26th',
+    '27th',
+    '28th',
+    '29th',
+    '30th',
+    '31st'
+];
+
 
 
 const twelveHourTime = false;
@@ -32,7 +66,9 @@ function displayTime() {
     const hour = theTime.getHours();
     const minute = theTime.getMinutes();
     const second = theTime.getSeconds();
-    const currentTime = `${hour}:${minute}:${second}`;
+    const twoDigitMinutes = minute < 10 ? minute.innerText = "0" + minute : theTime.getMinutes();
+    const twoDigitSeconds = second < 10 ? second.innerText = "0" + second : theTime.getSeconds();
+    const currentTime = `${hour}:${twoDigitMinutes}:${twoDigitSeconds}`;
     document.getElementsByClassName("clock")[0].innerHTML = currentTime;
     
 };
@@ -40,7 +76,7 @@ function displayTime() {
 function displayDate(){
     const theDate = new Date();
     const month = theMonth[theDate.getMonth()];
-    const date = theDate.getDate();
+    const date = todaysDate[theDate.getDate() - 1];
     const day = theDay[theDate.getDay()];
     const todayIs = `Today is ${day} ${month} ${date}`;
 /*
@@ -57,5 +93,5 @@ displayDate();
 displayTime();
 setInterval(displayTime, 1000);
 
-console.log(displayTime());
+console.log(displayTime);
 
